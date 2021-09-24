d7e5cd59c69b39e22772fa7d09a9aef907ebe624
Frank van Bakel <fvbakel@gmail.com>
2021-09-24 10:32:07 +0200

    Added script to process directory recursive

---
+++ nonogram/process_dir.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+solver=~/bin/nonogram
+
+if [ -n "$1" ]; then
+  echo "Processing directory: $1"
+  process_dir=$1
+else
+  echo "First parameter not supplied."
+  exit
+fi
+
+#FILES=`find -L ~/git/nonogram-db -name "*.non"`
+FILES=`find -L ${process_dir} -name "*.non"`
+for file in $FILES
+do
+    CMD="${solver} ${file}"
+    echo $CMD
+    $CMD
+done
+
+
+
+
