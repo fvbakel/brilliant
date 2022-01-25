277cd8ac8f5e0d4a4da2f1d1c44f58daa59ef7ec
Frank van Bakel <fvbakel@gmail.com>
2022-01-25 11:46:13 +0100

    Minimal loading of a model is working

--- hello_nodejs_vs/rollup_003/src/App.js
+++ hello_nodejs_vs/rollup_003/src/App.js
@@ -4,16 +4,20 @@ import {
 	MeshBasicMaterial,
 	PerspectiveCamera,
 	Scene,
+	Color,
 	WebGLRenderer,
 	EdgesGeometry,
 	LineBasicMaterial,
 	LineSegments,
 	Object3D,
-	SphereBufferGeometry
+	SphereBufferGeometry,
+	Box3
 } from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
 import { Creature } from '../src/Creature.mjs';
 import { Coordinate } from '../src/Coordinate.mjs';
 
@@ -27,6 +31,7 @@ class App {
 		this.camera.position.z = 400;
 
 		this.scene = new Scene();
+		this.scene.background = new Color( 0xffffff );
 
 		this.create_Box();
 
@@ -37,7 +42,7 @@ class App {
 
 		window.addEventListener( 'resize', onWindowResize, false );
 
-		const controls = new OrbitControls( this.camera, this.renderer.domElement );
+		this.controls = new OrbitControls( this.camera, this.renderer.domElement );
 
 		this.create_Creature();
 		this.create_GUI();
@@ -67,21 +72,120 @@ class App {
 		const geometry = new SphereBufferGeometry(100);
 		const material = new MeshBasicMaterial();
 		const mesh = new Mesh(geometry, material);
-		this.main_object.add(mesh);
 
+		this.main_object.add(mesh);
+		this.create_edges(this.main_object, mesh);
+
+		this.scene.add(this.main_object);
+	}
+
+	create_edges(parentObject3D, mesh) {
 		// add lines for the edges
-		const geo = new EdgesGeometry(geometry); // or WireframeGeometry( geometry )
+		const geo = new EdgesGeometry(mesh.geometry); // or WireframeGeometry( geometry )?
 		const mat = new LineBasicMaterial({ color: 0x000000, linewidth: 2 });
 
 		const wireframe = new LineSegments(geo, mat);
-		this.main_object.add(wireframe);
+		parentObject3D.add(wireframe);
+	}
+
+	activate_model() {
+		this.main_object = this.load_model('https://fvbakel.github.io/web-xr-hello/assets/2x4x3.gltf');
+		for (const child of this.main_object.children) {
+			if (child.isMesh) {
+				this.create_edges(this.main_object ,child);
+			}
+		}
+		
 		this.scene.add(this.main_object);
+	}
+
+	load_model(url) {
+		const loaded_model = new Object3D();
+		const loader = new GLTFLoader();
+		loader.load( url, ( gltf ) => {
+			loaded_model.add(gltf.scene);
+			/*gltf.scene.traverse( (child) => {
+				if (child.isMesh) {
+					loaded_model.add(child);
+					if (child.material === undefined) {
+						this.assign_default_material(child);
+					} else {
+						console.log('Using model defined material');
+					}
+				}
+			} );
+		/*	for (const child of gltf.scene.children) {
+				if (child.isMesh) {
+					loaded_model.add(child);
+					if (child.material === undefined) {
+						this.assign_default_material(child);
+					} else {
+						console.log('Using model defined material');
+					}
+				}
+			}
+			*/
+
+		} );
+
+
+		return loaded_model;
+	}
+
+	assign_default_material(mesh) {
+		const material = new MeshBasicMaterial( {
+			color: 0x00ff00,
+			wireframe: false
+		} );
+		mesh.material = material;
+		console.log('Default material assigned');
+	}
+
+	fitCameraToObject() {
+
+		const offset = 1.25;
+	//	const boundingBox = new Box3();
+	
+		// get bounding box of object - this will be used to setup controls and camera
+		
+	//	boundingBox.setFromObject( this.main_object );
+		
+		const center = this.main_object.getCenter();
+		const size = this.main_object.getSize();
+	
+		// get the max side of the bounding box (fits to width OR height as needed )
+		const maxDim = Math.max( size.x, size.y, size.z );
+		const fov = camera.fov * ( Math.PI / 180 );
+		let cameraZ = Math.abs( maxDim / 4 * Math.tan( fov * 2 ) );
+	
+		cameraZ *= offset; // zoom out a little so that objects don't fill the screen
+	
+		this.camera.position.z = cameraZ;
+	
+		const minZ = this.main_object.min.z; // boundingBox.min.z;
+		const cameraToFarEdge = ( minZ < 0 ) ? -minZ + cameraZ : cameraZ - minZ;
+	
+		this.camera.far = cameraToFarEdge * 3;
+		this.camera.updateProjectionMatrix();
+	
+		if ( this.controls ) {
+			// set camera to rotate around center of loaded object
+			this.controls.target = center;
+
+			// prevent camera from zooming out far enough to create far plane cutoff
+			this.controls.maxDistance = cameraToFarEdge * 2;
+			this.controls.saveState();
+			console.log('Controls updated');
+		} else {
+			this.camera.lookAt( center );
+			console.log('Camera updated');
+	    }
 	}
 
 	create_GUI() {
 		
 		this.current_shape = { name: 'Box' };
-		const shapes = [ 'Box', 'Sphere' ];
+		const shapes = [ 'Box', 'Sphere','Load model' ];
 		
 		this.gui = new GUI();
 		const shape_folder = this.gui.addFolder('shape'); 
@@ -101,7 +205,10 @@ class App {
 			this.create_Box();
 		} else if (this.current_shape.name === 'Sphere') {
 			this.create_Sphere();
+		} else if ( this.current_shape.name === 'Load model') {
+			this.activate_model();
 		}
+		//this.fitCameraToObject();
 	}
 
 	create_Creature() {
