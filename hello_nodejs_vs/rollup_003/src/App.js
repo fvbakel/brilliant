be12068e86141c765ff1628ab17a952371eba88f
Frank van Bakel <fvbakel@gmail.com>
2022-01-28 09:49:13 +0100

    Zoom to fit after load

--- hello_nodejs_vs/rollup_003/src/App.js
+++ hello_nodejs_vs/rollup_003/src/App.js
@@ -1,18 +1,24 @@
-import {
+/*import {
 	BoxBufferGeometry,
 	Mesh,
 	MeshBasicMaterial,
+//	MeshPhongMaterial, because that requires light
 	PerspectiveCamera,
 	Scene,
 	Color,
 	WebGLRenderer,
 	EdgesGeometry,
+//	WireframeGeometry,
 	LineBasicMaterial,
 	LineSegments,
 	Object3D,
 	SphereBufferGeometry,
+	AxesHelper,
 	BufferGeometry
 } from 'three';
+*/
+
+import * as THREE from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
@@ -20,6 +26,8 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import  * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 import { Creature } from '../src/Creature.mjs';
 import { Coordinate } from '../src/Coordinate.mjs';
+import { Vector3 } from 'three';
+import { AxisHelper } from 'three';
 
 //let camera, scene, renderer;
 
@@ -27,15 +35,28 @@ class App {
 
 	init() {
 
-		this.camera = new PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+		this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+		// In threejs the Y axis is up, unlike in all CAD Tools where Z is up
+		// in GLTF Y is also the default up axis
+	/*	this.camera.up.set( 0, 0, 1 );
+		THREE.Object3D.DefaultUp.x = 0;
+		THREE.Object3D.DefaultUp.y = 0;
+		THREE.Object3D.DefaultUp.z = 1;
+		*/
+
+		this.camera.position.x = 400;
+		this.camera.position.y = 400;
 		this.camera.position.z = 400;
+		
 
-		this.scene = new Scene();
-		this.scene.background = new Color( 0xffffff );
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color( 0xffffff );
 
+		this.create_material();
 		this.create_Box();
+		this.create_Axes();
 
-		this.renderer = new WebGLRenderer( { antialias: true } );
+		this.renderer = new THREE.WebGLRenderer( { antialias: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
 		document.body.appendChild( this.renderer.domElement );
@@ -49,23 +70,38 @@ class App {
 		animate();
 	}
 
+	create_material() {
+		this.material = new THREE.MeshBasicMaterial( {
+			color: 0xffffff,
+			polygonOffset: true,
+			polygonOffsetFactor: 0.5, // positive value pushes polygon further away
+			polygonOffsetUnits: 1
+		} );
+	}
+
+	create_Axes() {
+		this.axesHelper = new THREE.AxesHelper(5);
+		this.axesHelper.l
+		this.scene.add(this.axesHelper);
+	}
+
 	create_Box() {
-		this.main_object = new Object3D();
+		this.main_object = new THREE.Object3D();
 		// mesh
-		const geometry = new BoxBufferGeometry(200, 200, 200);
-		const material = new MeshBasicMaterial();
-		const mesh = new Mesh(geometry, material);
+		const geometry = new THREE.BoxBufferGeometry(200, 200, 200);
+		//const material = new MeshBasicMaterial();
+		const mesh = new THREE.Mesh(geometry, this.material);
 		this.main_object.add(mesh);
 		this.create_edges(this.main_object, mesh.geometry);
 		this.scene.add(this.main_object);
 	}
 
 	create_Sphere() {
-		this.main_object = new Object3D();
+		this.main_object = new THREE.Object3D();
 		// mesh
-		const geometry = new SphereBufferGeometry(100);
-		const material = new MeshBasicMaterial();
-		const mesh = new Mesh(geometry, material);
+		const geometry = new THREE.SphereBufferGeometry(100);
+		//const material = new MeshBasicMaterial();
+		const mesh = new THREE.Mesh(geometry, this.material);
 
 		this.main_object.add(mesh);
 		this.create_edges(this.main_object, mesh.geometry);
@@ -75,10 +111,12 @@ class App {
 
 	create_edges(parentObject3D, geometry) {
 		// add lines for the edges
-		const geo = new EdgesGeometry(geometry); // or WireframeGeometry( geometry )?
-		const mat = new LineBasicMaterial({ color: 0x000000, linewidth: 2 });
+		const geo = new THREE.EdgesGeometry(geometry,1); 
+		// not Wireframe because that gives lots of extra lines ....
+	//	const geo = new WireframeGeometry(geometry); // or WireframeGeometry( geometry )?
+		const mat = new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 2 });
 
-		const wireframe = new LineSegments(geo, mat);
+		const wireframe = new THREE.LineSegments(geo, mat);
 		parentObject3D.add(wireframe);
 	}
 
@@ -88,7 +126,7 @@ class App {
 	}
 
 	load_model(url) {
-		const loaded_model = new Object3D();
+		const loaded_model = new THREE.Object3D();
 		
 		const loader = new GLTFLoader();
 		loader.load( url, ( gltf ) => {
@@ -99,11 +137,16 @@ class App {
 					tmpGeomArray.push(child.geometry);
 				}
 			} );
-			const material = new MeshBasicMaterial();
+		//	const material = new MeshBasicMaterial();
 			const geometry = BufferGeometryUtils.mergeBufferGeometries(tmpGeomArray,false);
-			const mesh = new Mesh(geometry, material);
+			const mesh = new THREE.Mesh(geometry, this.material);
 			loaded_model.add(mesh);
 			this.create_edges(loaded_model ,geometry);
+			const axesHelper = new THREE.AxesHelper(3);
+			loaded_model.add(axesHelper);
+		//	loaded_model.position = new Vector3(0,0,0);
+
+			this.fitCameraToObject(mesh);
 		/*	for (const mesh of tmpMeshArray) {
 				loaded_model.add(mesh);
     			this.assign_default_material(mesh);
@@ -117,48 +160,50 @@ class App {
 	}
 
 	assign_default_material(mesh) {
-		const material = new MeshBasicMaterial( {
-			color: 0xffffff,
-			wireframe: false
-		} );
-		mesh.material = material;
+		mesh.material = this.material;
 		console.log('Default material assigned');
 	}
 
-	fitCameraToObject() {
-
-		const offset = 1.25;
+	fitCameraToObject(mesh) {
+		const offset = 2;
 	//	const boundingBox = new Box3();
 	
-		// get bounding box of object - this will be used to setup controls and camera
-		
+		if ( mesh.geometry.boundingBox == null ) mesh.geometry.computeBoundingBox();
+		if ( mesh.geometry.boundingSphere == null ) mesh.geometry.computeBoundingSphere();
 	//	boundingBox.setFromObject( this.main_object );
-		
-		const center = this.main_object.getCenter();
-		const size = this.main_object.getSize();
+		let boundingBox = mesh.geometry.boundingBox;
+
+		const wcs_center = new THREE.Vector3(0,0,0);
+		const center = boundingBox.getCenter(wcs_center);
+		const size   = boundingBox.getSize(wcs_center);
 	
 		// get the max side of the bounding box (fits to width OR height as needed )
 		const maxDim = Math.max( size.x, size.y, size.z );
-		const fov = camera.fov * ( Math.PI / 180 );
-		let cameraZ = Math.abs( maxDim / 4 * Math.tan( fov * 2 ) );
+		const fov 	 = this.camera.fov * ( Math.PI / 180 );
+		let cameraZ  = Math.abs( maxDim / 4 * Math.tan( fov * 2 ) );
 	
 		cameraZ *= offset; // zoom out a little so that objects don't fill the screen
 	
-		this.camera.position.z = cameraZ;
+	//	this.camera.position.z = cameraZ;
 	
-		const minZ = this.main_object.min.z; // boundingBox.min.z;
+		const minZ = boundingBox.min.z;
 		const cameraToFarEdge = ( minZ < 0 ) ? -minZ + cameraZ : cameraZ - minZ;
 	
-		this.camera.far = cameraToFarEdge * 3;
-		this.camera.updateProjectionMatrix();
-	
+	//	this.camera.far = cameraToFarEdge * 3;
+	//	this.camera.updateProjectionMatrix();
+	//	this.camera.lookAt( center );
+		this.camera.position.x = maxDim * offset;
+		this.camera.position.y = maxDim * offset;
+		this.camera.position.z = maxDim * offset;
+		center.z = -center.z;
 		if ( this.controls ) {
 			// set camera to rotate around center of loaded object
 			this.controls.target = center;
 
 			// prevent camera from zooming out far enough to create far plane cutoff
-			this.controls.maxDistance = cameraToFarEdge * 2;
-			this.controls.saveState();
+		//	this.controls.maxDistance = cameraToFarEdge * 2;
+		//	this.controls.saveState();
+			this.controls.update();
 			console.log('Controls updated');
 		} else {
 			this.camera.lookAt( center );
@@ -185,6 +230,12 @@ class App {
 
 	shapeChange() {
 		this.main_object.removeFromParent();
+		this.main_object.traverse( (child) => {
+			if (child.isMesh) {
+				child.geometry.dispose();
+				//child.material.dispose();
+			}
+		} );
 		if (this.current_shape.name === 'Box') {
 			this.create_Box();
 		} else if (this.current_shape.name === 'Sphere') {
@@ -192,7 +243,7 @@ class App {
 		} else if ( this.current_shape.name === 'Load model') {
 			this.activate_model();
 		}
-		//this.fitCameraToObject();
+		
 	}
 
 	create_Creature() {
@@ -223,7 +274,7 @@ function onWindowResize() {
 function animate() {
 
 	requestAnimationFrame( animate );
-	window.app.move_cycle();
+//	window.app.move_cycle();
 	window.app.renderer.render( window.app.scene, window.app.camera );
 
 }
